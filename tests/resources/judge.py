39173c5545d50cb293254c4d59c01e9e5a63f364
Aleksey Filippov <sarum9in@gmail.com>
2012-09-24 21:02:36 +0400

    tests/resources/judge.py bindings implementation.

--- tests/resources/judge.py
+++ tests/resources/judge.py
@@ -5,6 +5,80 @@ import time
 import math
 
 
+def escape(obj):
+    obj = str(obj)
+    buf = []
+    transform = {' ': '\\ ', '\n': '\\n', '\\': '\\\\'}
+    for c in obj:
+        buf.append(transform.get(c, c))
+    return "".join(buf)
+
+
+def join(*args):
+    return " ".join(escape(arg) for arg in args)
+
+
+def split(line):
+    tokens = []
+    buf = []
+    INIT = 0
+    ORDINAL = 1
+    ESCAPE = 2
+    state = INIT
+    transform = {'\\': '\\', 'n': '\n', ' ': ' '}
+    for c in line:
+        if state in {INIT, ORDINAL}:
+            if c == '\\':
+                state = ESCAPE
+            elif c in {' ', '\n'}:
+                tokens.append("".join(buf))
+                buf = []
+            else:
+                buf.append(c)
+        else:
+            assert state == ESCAPE, "Logic error"
+            assert c in transform, "Unknown character \"{}\"".format(c)
+            buf.append(transform[c])
+            state = ORDINAL
+    assert state != ESCAPE, "Last character may not be escape"
+    if len(buf):
+        tokens.append("".join(buf))
+    return tokens
+
+
+class ResourceLimits(object):
+
+    __slots__ = ('time_limit_millis', 'real_time_limit_millis')
+
+    def __init__(self, time_limit_millis, real_time_limit_millis):
+        self.time_limit_millis = time_limit_millis
+        self.real_time_limit_millis = real_time_limit_millis
+
+    def __str__(self):
+        return '{} {}'.format(self.time_limit_millis, self.real_time_limit_millis)
+
+    def __repr__(self):
+        return 'ResourceLimits(time_limit_millis={}, real_time_limit_millis={})'.format(
+            self.time_limit_millis, self.real_time_limit_millis)
+
+
+class Result(object):
+
+    __slots__ = ('status', 'data')
+
+    def __init__(self, status, data):
+        self.status = status
+        self.data = data
+
+    @staticmethod
+    def from_line(line):
+        return Result(*split(line))
+
+    def __repr__(self):
+        return "Result(status='{}', data='{}')".format(
+            self.status, self.data)
+
+
 def log(*args, **kwargs):
     kwargs["file"] = sys.stderr
     ftime, itime = math.modf(time.time())
@@ -14,5 +88,49 @@ def log(*args, **kwargs):
 
 
 def terminate(id):
-    log("Terminating {}".format(id))
-    print("TERMINATE {}".format(id))
+    log("Terminating", id)
+    print("TERMINATE", id)
+    sys.stdout.flush()
+
+
+def begin_1(id):
+    log("Begin", id)
+    print("BEGIN", id)
+    sys.stdout.flush()
+
+
+def begin_2(id, tokenizer_argument):
+    log("Begin", id, tokenizer_argument)
+    print("BEGIN", id, escape(tokenizer_argument))
+    sys.stdout.flush()
+
+
+def begin_3(id, tokenizer_argument, resource_limits):
+    log("Begin", id, tokenizer_argument, resource_limits)
+    assert isinstance(resource_limits, ResourceLimits),\
+        "resource_limits should be the instance of ResourceLimits"
+    print("BEGIN", id, escape(tokenizer_argument), escape(resource_limits))
+
+
+def begin(id, *args):
+    return [begin_1, begin_2, begin_3][len(args)](id, *args)
+
+
+def send(id, msg):
+    log("Send", id, msg)
+    print("SEND", id, escape(msg))
+    sys.stdout.flush()
+
+
+def end(id, discardRemaining=None):
+    print("END", id, end='')
+    if discardRemaining is not None:
+        log("End", id, discardRemaining)
+        print('', discardRemaining and 1 or 0)
+    else:
+        log("End", id)
+        print()
+    sys.stdout.flush()
+    result = Result.from_line(sys.stdin.readline())
+    log("Result.status =", result.status)
+    return result
