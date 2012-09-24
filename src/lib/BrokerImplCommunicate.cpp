edc27285ff44f2a331969a2c0f724f788b3c005c
Aleksey Filippov <sarum9in@gmail.com>
2012-09-24 23:22:54 +0400

    BrokerImpl::communicate() bug fixes + verbosity level increase.

--- src/lib/BrokerImplCommunicate.cpp
+++ src/lib/BrokerImplCommunicate.cpp
@@ -2,6 +2,8 @@
 
 #include "yandex/contest/SystemError.hpp"
 
+#include "yandex/contest/detail/LogHelper.hpp"
+
 #include "yandex/contest/system/unistd/Operations.hpp"
 
 #include <algorithm>
@@ -16,32 +18,40 @@ namespace yandex{namespace contest{namespace invoker{namespace flowctl{namespace
 
     void BrokerImpl::communicate(const SolutionId id)
     {
+        STREAM_TRACE << "id = " << id << ".";
         Solution &sol = solution(id);
         BOOST_ASSERT_MSG(!sol.terminated, "Solution was already terminated!");
         BOOST_ASSERT_MSG(sol.tokenizer, "Begin was not called!");
         const unistd::Descriptor epfd = unistd::epoll_create1();
         constexpr std::size_t MAXEVENTS = 2;
         struct epoll_event ev, events[MAXEVENTS];
-        ev.events = EPOLLIN;
+        ev.events = EPOLLOUT;
         ev.data.fd = sol.process.in;
         unistd::epoll_ctl_add(epfd.get(), sol.process.in, ev);
-        ev.events = EPOLLOUT;
+        ev.events = EPOLLIN;
         ev.data.fd = sol.process.out;
         unistd::epoll_ctl_add(epfd.get(), sol.process.out, ev);
+        STREAM_TRACE << "Starting communicate loop...";
         while (sol.tokenizerStatus == Tokenizer::Status::CONTINUE)
         {
+            STREAM_TRACE << "Waiting for event(s)...";
             // TODO the last argument may be used in RTL control
             const std::size_t nfds = unistd::epoll_wait(epfd.get(), events);
+            STREAM_TRACE << "Found " << nfds << " events.";
             for (std::size_t i = 0; i < nfds; ++i)
             {
                 if (events[i].data.fd == sol.process.in)
                 {
-                    // may write
+                    STREAM_TRACE << "May write into " << sol.process.in << ".";
                     if (sol.tokenizerStatus != Tokenizer::Status::CONTINUE)
+                    {
+                        STREAM_TRACE << "Nothing to write into " << sol.process.in << ".";
                         continue; // TODO may be break?
+                    }
                     constexpr std::size_t BUFSIZE = 1024;
                     char buf[BUFSIZE];
-                    for (;;)
+                    STREAM_TRACE << "Starting write loop (fd = " << sol.process.in << ").";
+                    while (!sol.inbuf.empty())
                     {
                         const std::size_t bsize = std::min(BUFSIZE, sol.inbuf.size());
                         std::copy_n(sol.inbuf.begin(), bsize, buf);
@@ -49,17 +59,25 @@ namespace yandex{namespace contest{namespace invoker{namespace flowctl{namespace
                         if (size < 0)
                         {
                             if (errno == EAGAIN)
+                            {
+                                STREAM_TRACE << "Unable to write more into " <<
+                                                sol.process.in << ".";
                                 break;
+                            }
                             else
+                            {
                                 BOOST_THROW_EXCEPTION(SystemError("write") <<
                                                       unistd::info::fd(sol.process.in));
+                            }
                         }
+                        STREAM_TRACE << "Written " << size << " bytes into " <<
+                                        sol.process.in << ".";
                         sol.inbuf.erase(sol.inbuf.begin(), sol.inbuf.begin() + size);
                     }
                 }
                 else if (events[i].data.fd == sol.process.out)
                 {
-                    // may read
+                    STREAM_TRACE << "May read from " << sol.process.out << ".";
                     constexpr std::size_t BUFSIZE = 1024;
                     char buf[BUFSIZE];
                     ssize_t size;
